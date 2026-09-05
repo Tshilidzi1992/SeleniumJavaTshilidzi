9eca19b484af18627504fc6838e7f7cf4782d53c
Tshilidzi Neluvhalani <tneluvhalani@gmail.com>
2026-09-05 18:07:24 +0200

    created a login button locator

---
+++ src/test/java/Pages/homePage.java
@@ -0,0 +1,17 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class homePage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[text()='Login']")
+    WebElement loginButton;
+
+    public homePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
